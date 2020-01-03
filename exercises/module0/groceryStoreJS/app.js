f21131c6e25fd7fbcf6c90cae8fdb5abb13c322a
Fernando Estrada <estradam.fernando@gmail.com>
2020-01-03 14:04:01 -0700

    Start ifStatementOlympics

--- exercises/module0/groceryStoreJS/app.js
+++ exercises/module0/groceryStoreJS/app.js
@@ -2,5 +2,15 @@ var shopper = {
     name: "Fernando",
     age: 23,
     isAlive: true,
-    
+    aboutMe: function() {
+        if (this.isAlive === true){
+        return this.name + " is " + this.age + " and he is alive."
+        }
+        else{
+        return this.name + " is " + this.age + " and he is not alive."
+        }
+    },
+    groceryCart: ['milk', 'bread', 'eggs', 'meat','juice']
 }
+console.log(shopper.aboutMe())
+console.log(shopper.groceryCart)
