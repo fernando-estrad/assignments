fc5f21b60384dc4b8a46b01c3b77db428c1e3481
Fernando Estrada <estradam.fernando@gmail.com>
2020-01-16 10:56:07 -0700

    finished problem-solving-practice.

---
+++ exercises/module1/problem-solving-practice/app.js
@@ -0,0 +1,34 @@
+// numArr = [5, 6, 11, 33, 4]
+// function returnLargest(arr){
+//     let largest 
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] > largest || typeof largest === "undefined"){
+//         largest = arr[i
+//         }
+//     }
+//     return largest
+// }
+// console.log(returnLargest(numArr))
+
+
+// randomArr = ["#3", "$$$", "C%4!", "Hey!", "$77r"]
+// matchingArr = []
+// function match (arr, str){
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i].includes(str)){
+//             matchingArr.push(arr[i])
+//         }
+//     }
+//     return matchingArr
+// }
+// console.log(match(randomArr, "$"))
+
+// function isDivisible(num1, num2){
+//     if (num1 % num2 == 0){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// console.log(isDivisible(2, 3))
