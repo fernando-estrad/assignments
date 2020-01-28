19a8871d10772762fdf9bd01fd4d3afe410812ca
Fernando Estrada <estradam.fernando@gmail.com>
2020-01-28 16:28:53 -0700

    Finished pokemon-list

---
+++ exercises/module2/pokemon-list/app.js
@@ -0,0 +1,24 @@
+const xhr = new XMLHttpRequest()
+const list = document.getElementById("list")
+
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+xhr.onreadystatechange = function(){
+    if (xhr.status === 200 && xhr.readyState === 4){
+        const JSONdata = xhr.responseText
+        const data = JSON.parse(JSONdata)
+        const pokeArr = data.objects[0].pokemon
+        listPokemon(pokeArr)
+    }
+}
+
+function listPokemon(pokeArr){
+    for (i = 0; i < pokeArr.length; i++){
+        const pokemonName = pokeArr[i].name
+        const h3 = document.createElement("h3")
+        h3.innerHTML = `<li> ${pokemonName} </li>`
+        list.append(h3)
+    }
+}
+
