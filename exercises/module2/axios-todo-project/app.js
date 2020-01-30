d8ab29dcfd50895a1321accef15a9f37a5853709
Fernando Estrada <estradam.fernando@gmail.com>
2020-01-29 17:47:40 -0700

    Starting styling of axios-todo, basic layout.

--- exercises/module2/axios-todo-project/app.js
+++ exercises/module2/axios-todo-project/app.js
@@ -1,5 +1,11 @@
 const axios = require('axios')
 
+const newTodo = {
+    title: "This is the first one",
+    description: "Random description",
+    imgUrl: "randomwebsite.com/img.jpg"
+}
+
 axios.get("https://api.vschool.io/fernandoestrada/todo")
     .then(response => console.log(response.data))
     .catch(error => console.log(error))
