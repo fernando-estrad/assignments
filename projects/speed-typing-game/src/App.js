ed7294e1457d2ac08a479e67021c7d1a11d7dedb
Fernando Estrada <estradam.fernando@gmail.com>
2020-03-19 17:14:47 -0600

    Starting speed-typing-game

---
+++ projects/speed-typing-game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>How Fast Can You Type?</h1>
+      <textarea />
+      <h4>0</h4>
+    </div>
+  )
+}
+
+export default App;
